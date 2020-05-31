17cc69dc816ed4a3e661c335217c15b4584c8953
Julius <Julius>
2020-05-31 10:56:25 +0300

    new commit

---
+++ src/webserver/MY_WebServer.java
@@ -0,0 +1,13 @@
+package webserver;
+
+/**
+ *
+ * @author Julius
+ */
+public class MY_WebServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
